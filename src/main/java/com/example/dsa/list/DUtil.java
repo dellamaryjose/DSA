e270c05333550c400be42296589a812e627078d5
Della Mary Jose <dellamaryjose@gmail.com>
2024-04-19 09:20:48 -0400

    LinkedList updates

---
+++ src/main/java/com/example/dsa/list/DUtil.java
@@ -0,0 +1,27 @@
+package com.example.dsa.list;
+
+import com.example.dsa.list.DoublyLinkedList.Node;
+
+public class DUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+DoublyLinkedList<Integer> dl = new DoublyLinkedList<>();
+Node<Integer> node1 = new Node<Integer>(2);
+Node<Integer> node2 = new Node<Integer>(4);
+Node<Integer> node3 = new Node<Integer>(8);
+/*
+ * dl.head = node1; dl.head.next = node2; node2.next = node3; dl.tail = node3;
+ * System.out.println(dl);
+ */
+//dl.insertFirst(1);
+//System.out.println(dl);
+dl.insertLast(6);
+dl.insertLast(2);
+System.out.println(dl);
+
+dl.deleteFirst();
+System.out.println(dl);
+	}
+
+}
---
+++ src/main/java/com/example/dsa/list/DoublyLinkedList.java
@@ -0,0 +1,86 @@
+package com.example.dsa.list;
+
+import java.util.NoSuchElementException;
+
+public class DoublyLinkedList<T> {
+	Node<T> head;
+	Node<T> tail;
+
+	static class Node<T> {
+
+		T element;
+		Node<T> previous;
+		Node<T> next;
+
+		public Node(T element) {
+
+			this.element = element;
+		}
+	}
+
+
+	public String toString() {
+		
+		Node<T> currentNode = head;
+		String msg="";
+		while(currentNode!=null) {
+			
+			msg = msg+"-->"+currentNode.element;
+			currentNode = currentNode.next;
+		}
+		msg = msg+"-->null";
+		return msg;
+		
+		
+		
+	}
+	
+	public void insertFirst(T element) {
+		
+		Node<T> newNode = new Node<T>(element);
+		if(head==null && tail == null) {
+			
+			tail = newNode;
+		}
+		else {
+			
+			head.previous = newNode;
+		}
+		newNode.next=head;
+		head = newNode;
+		
+	}
+	
+	public void insertLast(T element) {
+		Node<T> newNode = new Node<>(element);
+		if(head==null ||tail==null) {
+			head=newNode;
+			
+		}else {
+			
+			tail.next = newNode;
+		}
+		newNode.previous = tail;
+		tail=newNode;
+	}
+public Node<T> deleteFirst() {
+	
+	if(head==null) {
+		
+		throw new NoSuchElementException();
+	}
+	Node<T> temp = head;
+	if(head==tail) {
+		
+		tail=null;
+	}
+	else {
+		head.next.previous = null;
+		
+		
+	}
+	head = head.next;
+	temp.next = null;
+	return temp;
+}
+}
